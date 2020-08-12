e47191415148c195f8b3dc346d205fa87c78f415
gamehackerever <gamehackerever@gmail.com>
2020-08-12 08:54:24 +0000

    vince: Set Gapps Flag

--- ancient_vince.mk
+++ ancient_vince.mk
@@ -40,3 +40,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := "google/coral/coral:10/QQ3A.200705.002/6506677:user/release-keys"
+
+# Gapps
+TARGET_GAPPS_ARCH := arm64
