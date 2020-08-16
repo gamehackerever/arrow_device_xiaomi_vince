46b83631d5e24fd9f5a76358b39ce9fa0f9c8777
KenHV <kenhv@null.net>
2020-08-16 15:44:05 +0530

    vince: rootdir: update usb configs from land

--- device.mk
+++ device.mk
@@ -334,6 +334,7 @@ PRODUCT_PACKAGES += \
     init.qcom.rc \
     init.qcom.sh \
     init.qcom.usb.rc \
+    init.qcom.usb.sh \
     init.target.rc \
     init.xiaomi_parts.rc \
     ueventd.qcom.rc \
